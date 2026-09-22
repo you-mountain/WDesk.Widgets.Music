723a66739fd2a46f3c67001c51be92ea417f1382
love-sick <ssajad2153@gmail.com>
2026-09-22 11:06:16 +0330

    Release v1.0.0

---
+++ Style/Style3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WDesk.Widgets.Music.Style
+{
+    internal class Style3
+    {
+    }
+}
